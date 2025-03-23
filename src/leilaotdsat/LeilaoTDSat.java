d895f0dd9da6d2149375ce33b78db920a0f98f59
Vinicius <carlosviniciusdejesusandrade@gmail.com>
2025-03-23 14:25:45 -0300

    Projeto inicial

---
+++ src/leilaotdsat/LeilaoTDSat.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package leilaotdsat;
+
+/**
+ *
+ * @author Vinni
+ */
+public class LeilaoTDSat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
